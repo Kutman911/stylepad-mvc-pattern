12f56f00bfae2f42ecfed3b6be827307e47b49cb
Айдина Суюмбаева <voodoo.people.j@gmail.com>
2025-11-05 05:47:17 +0600

    stylized the GUI

--- src/Viewer.java
+++ src/Viewer.java
@@ -10,9 +10,14 @@ import javax.swing.KeyStroke;
 import java.awt.event.KeyEvent;
 import java.awt.event.ActionEvent;
 import javax.swing.ImageIcon;
+import javax.swing.JSeparator;
+import javax.swing.BorderFactory;
+import javax.swing.UIManager;
 
 
 public class Viewer {
+
+  private JTextArea textArea;
 
     public Viewer() {
       Controller controller = new Controller(this);
@@ -20,49 +25,109 @@ public class Viewer {
     }
 
     private void createGUI(Controller controller) {
+      
+      UIManager.put("MenuBar.background", new Color(255, 204, 232));
+      UIManager.put("MenuBar.foreground", Color.WHITE);
+      UIManager.put("MenuBar.border", BorderFactory.createMatteBorder(0, 0, 2, 0, new Color(255, 105, 180)));
+      UIManager.put("Menu.selectionBackground", new Color(255, 105, 180));
+      UIManager.put("Menu.selectionForeground", Color.WHITE);
+      UIManager.put("MenuItem.selectionBackground", new Color(255, 160, 200));
+      UIManager.put("MenuItem.selectionForeground", Color.WHITE);
+      UIManager.put("MenuItem.background", new Color(255, 228, 240));
+      UIManager.put("MenuItem.foreground", new Color(199, 21, 133));
 
-        JMenuBar menuBar = createJMenuBar(controller);
+      JMenuBar menuBar = createJMenuBar(controller);
 
-        Font fontTextArea = new Font("Bernard MT Condensed", Font.PLAIN, 35);
-        Color colorTextArea = Color.BLACK;
-        JTextArea textArea = new JTextArea("Center");
-        textArea.setFont(fontTextArea);
-        textArea.setForeground(colorTextArea);
-        JScrollPane scrollPane = new JScrollPane(textArea);
+      Font fontTextArea = new Font("JetBrains Mono", Font.PLAIN, 16);
+      Color colorTextArea = Color.BLACK;
 
-        JFrame frame = new JFrame("Stylepad MVC Pattern");
-        frame.setSize(800, 800);
-        frame.setLocation(500, 150);
-        frame.setJMenuBar(menuBar);
-        frame.add("Center", scrollPane);
-        frame.setVisible(true);
-        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+      textArea = new JTextArea();
+      textArea.setFont(fontTextArea);
+      textArea.setForeground(colorTextArea);
+      textArea.setBackground(Color.WHITE);
+      textArea.setCaretColor(new Color(199, 21, 133));
+      textArea.setSelectedTextColor(Color.WHITE);
+      textArea.setSelectionColor(new Color(255, 105, 180));
+      textArea.setBorder(BorderFactory.createCompoundBorder(BorderFactory.createMatteBorder(6, 6, 6, 6, new Color(255, 204, 232)),BorderFactory.createEmptyBorder(8, 10, 8, 10)));
+
+      JScrollPane scrollPane = new JScrollPane(textArea);
+      scrollPane.setBorder(BorderFactory.createMatteBorder(0,2,2,2, new Color(255, 105, 180)));
+
+
+
+      JFrame frame = new JFrame("StylePad MVC Pattern");
+      frame.setSize(500, 500);
+      frame.setLocation(320, 140);
+      frame.setJMenuBar(menuBar);
+      frame.add("Center", scrollPane);
+      frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+      frame.setVisible(true);
+
     }
 
     private JMenuBar createJMenuBar(Controller controller) {
+      JMenuBar menuBar = new JMenuBar();
 
-        JMenu fileMenu = createFileMenu(controller);
+      JMenu fileMenu = createFileMenu(controller);
+      menuBar.add(fileMenu);
 
-        JMenuBar menuBar = new JMenuBar();
-        menuBar.add(fileMenu);
-        return menuBar;
+      return menuBar;
     }
+
 
     private JMenu createFileMenu(Controller controller) {
 
-        JMenuItem newDocument = new JMenuItem("Новый", new ImageIcon("images/new.gif"));
-        newDocument.setAccelerator(KeyStroke.getKeyStroke(KeyEvent.VK_N, ActionEvent.CTRL_MASK));
-        newDocument.addActionListener(controller);
-        newDocument.setActionCommand("New_Document");
+      JMenuItem newDocument = new JMenuItem("New",new ImageIcon("images/new.png"));
+      newDocument.setAccelerator(KeyStroke.getKeyStroke(KeyEvent.VK_N, ActionEvent.CTRL_MASK));
+      newDocument.addActionListener(controller);
+      newDocument.setActionCommand("New_Document");
 
-        JMenuItem openDocument = new JMenuItem("Открыть ...", new ImageIcon("images/open.gif"));
-        openDocument.setAccelerator(KeyStroke.getKeyStroke(KeyEvent.VK_O, ActionEvent.CTRL_MASK));
-        openDocument.addActionListener(controller);
-        openDocument.setActionCommand("Open_Document");
+      JMenuItem openDocument = new JMenuItem("Open ...", new ImageIcon("images/open.gif"));
+      openDocument.setAccelerator(KeyStroke.getKeyStroke(KeyEvent.VK_O, ActionEvent.CTRL_MASK));
+      openDocument.addActionListener(controller);
+      openDocument.setActionCommand("Open_Document");
+      JMenuItem saveDocument = new JMenuItem("Save", new ImageIcon("images/save.gif"));
+      saveDocument.setAccelerator(KeyStroke.getKeyStroke(KeyEvent.VK_S, ActionEvent.CTRL_MASK));
+      saveDocument.addActionListener(controller);
+      saveDocument.setActionCommand("Save_Document");
 
-        JMenu fileMenu = new JMenu("Файл");
-        fileMenu.add(newDocument);
-        fileMenu.add(openDocument);
-        return fileMenu;
+      JMenuItem saveAsDocument = new JMenuItem("Save As ...", new ImageIcon("images/save_as.gif"));
+      saveAsDocument.addActionListener(controller);
+      saveAsDocument.setActionCommand("Save_Document");
+
+      JMenuItem printDocument = new JMenuItem("Print ...", new ImageIcon("images/print.gif"));
+      printDocument.setAccelerator(KeyStroke.getKeyStroke(KeyEvent.VK_P, ActionEvent.CTRL_MASK));
+      printDocument.addActionListener(controller);
+      printDocument.setActionCommand("Print_Document");
+
+      JMenuItem imageDocument = new JMenuItem("Open Image", new ImageIcon("images/wordSpace.gif"));
+      imageDocument.setAccelerator(KeyStroke.getKeyStroke(KeyEvent.VK_I, ActionEvent.CTRL_MASK));
+      imageDocument.addActionListener(controller);
+      imageDocument.setActionCommand("Open_Image");
+
+      JMenuItem closeProgram = new JMenuItem("Exit");
+      closeProgram.addActionListener(controller);
+      closeProgram.setActionCommand("Exit");
+
+
+      JMenu fileMenu = new JMenu("File");
+      fileMenu.setMnemonic('F');
+      fileMenu.add(newDocument);
+      fileMenu.add(openDocument);
+      fileMenu.add(saveDocument);
+      fileMenu.add(saveAsDocument);
+      fileMenu.add(new JSeparator());
+      fileMenu.add(imageDocument);
+      fileMenu.add(new JSeparator());
+      fileMenu.add(printDocument);
+      fileMenu.add(new JSeparator());
+      fileMenu.add(closeProgram);
+
+      return fileMenu;
     }
+
+    public void update(String text) {
+      textArea.setText(text);
+    }
+
 }
