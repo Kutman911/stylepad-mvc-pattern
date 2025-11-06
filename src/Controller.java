e3a8a4115e2e59a40268393eca8b61dc70ad9b3b
Бекжан <legendaryouroboros@gmail.com>
2025-11-06 21:09:35 +0600

    added save logic

--- src/Controller.java
+++ src/Controller.java
@@ -1,15 +1,74 @@
 import java.awt.event.ActionListener;
 import java.awt.event.ActionEvent;
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.PrintWriter;
+
 public class Controller implements ActionListener {
 
-  private Viewer viewer;
+private Viewer viewer;
+private File currentFile;
 
-  public Controller(Viewer viewer) {
-      this.viewer = viewer;
+public Controller(Viewer viewer) {
+  this.viewer = viewer;
+}
+
+public void actionPerformed(ActionEvent event) {
+  String command = event.getActionCommand();
+
+  if (command.equals("New_Document")) {
+    File file = viewer.showFileDialog("Save");
+
+} else if (command.equals("Save_Document")) {
+    if (currentFile != null) {
+      String text = viewer.contentTextArea();
+      boolean b = saveToFile(currentFile, text);
+      if (b) {
+          System.out.println("Saved to file");
+      } else {
+          System.out.println("Failed to save to file");
+      }
+  } else {
+      File file = viewer.showFileDialog("Save");
+      if (file != null) {
+          currentFile = file;
+          String text = viewer.contentTextArea();
+          boolean b = saveToFile(file, text);
+          if (b) {
+              System.out.println("Saved to file");
+          } else {
+              System.out.println("Failed to save to file");
+          }
+        }
+      }
+  } else if (command.equals("SaveAs_Document")) {
+      File file = viewer.showFileDialog("Save");
+      if (file != null) {
+          currentFile = file;
+          String text = viewer.contentTextArea();
+          saveToFile(file, text);
+      }
   }
 
-  public void actionPerformed(ActionEvent event) {
-      String command = event.getActionCommand();
-      System.out.println(command);
-  }
+}
+private boolean saveToFile(File file, String text) {
+
+    PrintWriter out = null;
+
+    try {
+        out = new PrintWriter(new FileWriter(file));
+        out.println(text);
+        out.flush();
+        return true;
+    } catch (IOException ioe) {
+        System.out.println(ioe);
+        return false;
+    } finally {
+        if (out != null) {
+            out.close();
+        }
+    }
+
+ }
 }
