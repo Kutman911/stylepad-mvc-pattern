0577f30d4a4c37d0c2c44a17b80af3b41b02c4a2
Kylychbek <kylychbekdata@gmail.com>
2025-11-14 01:26:22 +0600

    fixed wrapHandler

--- src/WrapHandler.java
+++ src/WrapHandler.java
@@ -1,87 +1,74 @@
 import javax.swing.JTextPane;
-import javax.swing.text.AbstractDocument;
-import javax.swing.text.BoxView;
-import javax.swing.text.ComponentView;
-import javax.swing.text.Document;
-import javax.swing.text.Element;
-import javax.swing.text.IconView;
-import javax.swing.text.LabelView;
-import javax.swing.text.ParagraphView;
-import javax.swing.text.StyleConstants;
-import javax.swing.text.StyledEditorKit;
-import javax.swing.text.View;
-import javax.swing.text.ViewFactory;
+import javax.swing.SwingUtilities;
+import javax.swing.text.*;
 
 public class WrapHandler extends CommandHandler {
-  private Viewer viewer;
-  private boolean isWrapEnabled = false;
 
-  public WrapHandler(Viewer viewer) {
-    this.viewer = viewer;
-  }
+    private Viewer viewer;
+    private boolean isWrapEnabled = true;
 
-  void command() {
-    isWrapEnabled = !isWrapEnabled;
-
-    JTextPane textPane = viewer.getTextPane();
-    Document currentDocument = textPane.getDocument();
-    int caretPosition = textPane.getCaretPosition();
-
-    if (isWrapEnabled) {
-      textPane.setEditorKit(new WrapEditorKit());
-    } else {
-      textPane.setEditorKit(new StyledEditorKit());
+    public WrapHandler(Viewer viewer) {
+        this.viewer = viewer;
     }
 
-    textPane.setDocument(currentDocument);
+    void command() {
+        isWrapEnabled = !isWrapEnabled;
 
-    if (caretPosition <= currentDocument.getLength()) {
-      textPane.setCaretPosition(caretPosition);
-    } else {
-      textPane.setCaretPosition(currentDocument.getLength());
+        JTextPane textPane = viewer.getTextPane();
+        Document currentDocument = textPane.getDocument();
+        int caretPosition = textPane.getCaretPosition();
+
+        if (isWrapEnabled) {
+            textPane.setEditorKit(new WrapEditorKit());
+        } else {
+            textPane.setEditorKit(new StyledEditorKit());
+        }
+
+        SwingUtilities.invokeLater(() ->
+            SwingUtilities.invokeLater(() -> {
+                textPane.setDocument(currentDocument);
+                textPane.setCaretPosition(Math.min(caretPosition, currentDocument.getLength()));
+                textPane.revalidate();
+                textPane.repaint();
+            })
+        );
     }
 
-    textPane.revalidate();
-    textPane.repaint();
-  }
-
-  static class WrapEditorKit extends StyledEditorKit {
-    public ViewFactory getViewFactory() {
-      return new CustomViewFactory();
+    static class WrapEditorKit extends StyledEditorKit {
+        public ViewFactory getViewFactory() {
+            return new CustomViewFactory();
+        }
     }
 
     static class CustomViewFactory implements ViewFactory {
-      public View create(Element element) {
-        String elementType = element.getName();
 
-        if (AbstractDocument.ContentElementName.equals(elementType)) {
-          return new WrapLabelView(element);
-        } else if (AbstractDocument.ParagraphElementName.equals(elementType)) {
-          return new ParagraphView(element);
-        } else if (AbstractDocument.SectionElementName.equals(elementType)) {
-          return new BoxView(element, View.Y_AXIS);
-        } else if (StyleConstants.ComponentElementName.equals(elementType)) {
-          return new ComponentView(element);
-        } else if (StyleConstants.IconElementName.equals(elementType)) {
-          return new IconView(element);
+        public View create(Element element) {
+            String type = element.getName();
+
+            if (AbstractDocument.ContentElementName.equals(type))
+                return new WrapLabelView(element);
+            if (AbstractDocument.ParagraphElementName.equals(type))
+                return new ParagraphView(element);
+            if (AbstractDocument.SectionElementName.equals(type))
+                return new BoxView(element, View.Y_AXIS);
+            if (StyleConstants.ComponentElementName.equals(type))
+                return new ComponentView(element);
+            if (StyleConstants.IconElementName.equals(type))
+                return new IconView(element);
+
+            return new LabelView(element);
+        }
+    }
+
+    static class WrapLabelView extends LabelView {
+        public WrapLabelView(Element element) {
+            super(element);
         }
 
-        return new LabelView(element);
-      }
+        public float getMinimumSpan(int axis) {
+            if (axis == View.X_AXIS)
+                return 0;
+            return super.getMinimumSpan(axis);
+        }
     }
-  }
-
-  static class WrapLabelView extends LabelView {
-    public WrapLabelView(Element element) {
-      super(element);
-    }
-
-    public float getMinimumSpan(int axis) {
-      if (axis == View.X_AXIS) {
-        return 0;
-      } else {
-        return super.getMinimumSpan(axis);
-      }
-    }
-  }
 }
