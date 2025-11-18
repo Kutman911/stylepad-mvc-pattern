3b1cad95b93ce8bfffd6e4cda7ba57e900cf729b
Махир Джамалов <mahirdjamalov@gmail.com>
2025-11-19 04:07:24 +0600

    refactor(controller) make refactoring

--- src/Controller.java
+++ src/Controller.java
@@ -14,15 +14,15 @@ public class Controller implements ActionListener {
 
   private Viewer viewer;
   private File currentFile;
-  private long lastModifiedTs;
-  private Timer syncTimer;
+  // private long lastModifiedTs;
+  // private Timer syncTimer;
   private Map<String, CommandHandler> map;
 
   public Controller(Viewer viewer) {
     this.viewer = viewer;
-    lastModifiedTs = -1;
-    syncTimer = new Timer(2000, new SyncTimerListener());
-    syncTimer.start();
+    // lastModifiedTs = -1;
+    // syncTimer = new Timer(2000, new SyncTimerListener());
+    // syncTimer.start();
     map = new HashMap<>();
     SaveHandler saveHandler = new SaveHandler(viewer);
     map.put("Save_Document", saveHandler);
@@ -59,46 +59,47 @@ public class Controller implements ActionListener {
 
   }
 
-  private String readFile(File file) throws IOException {
-    FileInputStream fileInputStream = null;
-    BufferedInputStream bufferedInputStream = null;
-    StringBuilder container = new StringBuilder();
-    try {
-      fileInputStream = new FileInputStream(file);
-      bufferedInputStream = new BufferedInputStream(fileInputStream);
-      int unicode = -1;
-      while ((unicode = bufferedInputStream.read()) != -1) {
-        char symbol = (char) unicode;
-        container.append(symbol);
-      }
-      return container.toString();
-    } finally {
-      if (bufferedInputStream != null) {
-        try { bufferedInputStream.close();
-        } catch (IOException ignore) {}
-      }
-      if (fileInputStream != null) {
-        try { fileInputStream.close();
-        } catch (IOException ignore) {}
-      }
-    }
-  }
+  // Вынеси в отдельный класс
+  // private String readFile(File file) throws IOException {
+  //   FileInputStream fileInputStream = null;
+  //   BufferedInputStream bufferedInputStream = null;
+  //   StringBuilder container = new StringBuilder();
+  //   try {
+  //     fileInputStream = new FileInputStream(file);
+  //     bufferedInputStream = new BufferedInputStream(fileInputStream);
+  //     int unicode = -1;
+  //     while ((unicode = bufferedInputStream.read()) != -1) {
+  //       char symbol = (char) unicode;
+  //       container.append(symbol);
+  //     }
+  //     return container.toString();
+  //   } finally {
+  //     if (bufferedInputStream != null) {
+  //       try { bufferedInputStream.close();
+  //       } catch (IOException ignore) {}
+  //     }
+  //     if (fileInputStream != null) {
+  //       try { fileInputStream.close();
+  //       } catch (IOException ignore) {}
+  //     }
+  //   }
+  // }
 
-  private class SyncTimerListener implements ActionListener {
-    public void actionPerformed(ActionEvent e) {
-      if (currentFile != null && currentFile.exists()) {
-        long ts = currentFile.lastModified();
-        if (ts != lastModifiedTs) {
-          try {
-            String content = readFile(currentFile);
-            viewer.update(content);
-            lastModifiedTs = ts;
-            System.out.println("File reloaded from disk: " + currentFile.getName());
-          } catch (IOException ex) {
-            System.out.println("Sync error: " + ex);
-          }
-        }
-      }
-    }
-  }
+  // private class SyncTimerListener implements ActionListener {
+  //   public void actionPerformed(ActionEvent e) {
+  //     if (currentFile != null && currentFile.exists()) {
+  //       long ts = currentFile.lastModified();
+  //       if (ts != lastModifiedTs) {
+  //         try {
+  //           String content = readFile(currentFile);
+  //           viewer.update(content);
+  //           lastModifiedTs = ts;
+  //           System.out.println("File reloaded from disk: " + currentFile.getName());
+  //         } catch (IOException ex) {
+  //           System.out.println("Sync error: " + ex);
+  //         }
+  //       }
+  //     }
+  //   }
+  // }
 }
